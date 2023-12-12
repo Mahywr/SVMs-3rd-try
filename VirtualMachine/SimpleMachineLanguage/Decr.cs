873cb04d12a56985e924ee93fc3a8d303453b7f6
mahywr <mahywr.yt@gmail.com>
2023-12-12 12:47:43 +0000

    Add project files.

---
+++ VirtualMachine/SimpleMachineLanguage/Decr.cs
@@ -0,0 +1,12 @@
+﻿namespace SVM.SimpleMachineLanguage;
+
+/// <summary>
+/// Implements the SML Decr  instruction
+/// Decrements the integer value stored on top of the stack, 
+/// leaving the result on the stack
+/// </summary>
+public class Decr
+{
+    #region TASK 3 - TO BE IMPLEMENTED BY THE STUDENT
+    #endregion
+}
---
+++ VirtualMachine/SimpleMachineLanguage/Incr.cs
@@ -0,0 +1,12 @@
+﻿namespace SVM.SimpleMachineLanguage;
+
+/// <summary>
+/// Implements the SML Incr  instruction
+/// Increments the integer value stored on top of the stack, 
+/// leaving the result on the stack
+/// </summary>
+public class Incr
+{
+    #region TASK 3 - TO BE IMPLEMENTED BY THE STUDENT
+    #endregion
+}
